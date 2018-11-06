3bb360789b0c2344750ca9f6de70abe9d00e7427
in6802 <in6802@gmail.com>
2018-11-06 17:22:42 +0900

    JI - order activity

--- app/src/main/java/compinner/cji/edu/aslijava/OrderActivity.java
+++ app/src/main/java/compinner/cji/edu/aslijava/OrderActivity.java
@@ -9,5 +9,12 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        //인텐드 받아오기
+
+
+        //메뉴 리스트 채우기
+
+
     }
 }
